ae4a4ae1d2b75b752f056200c8e166685af160f4
MrtMts <mrmetsallik@gmail.com>
2024-02-05 22:54:17 +0200

    add wee2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// rakendus palub kasutajal valida sugu (m/f)
+// rakendus palub kasutajal sisestada perekonnanime
+// vastavalt sisestatule, rakendus tervitab kasutajat:
+// "Welcome, Mr. [kasutaja perekonnanimi] " / "Welcome, Ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please select your gender (m/f)");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis
+
+Console.WriteLine("Please enter your last name:");
+
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms.{userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+
+
---
+++ userAgeValidation/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+// rakendus palub kasutajal sisestada oma vanus
+// kui kasutaja vanus on väiksem, kui 13, siis konsoolis kuvatakse:
+//"You are too young to use Instagaram"
+//muul juhul
+//kuvatakse konsoolis "Welcome to Instagaram"
+
+Console.WriteLine("Enter your age:"  );
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+//boolean true/false
+
+bool isAgeNumber = Int32.TryParse( userAge, out userAgeNum );
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if( isAgeNumber )
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagaram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagaram!");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age!");
+}
+
+
+//int userAge = Int32.Parse(Console.ReadLine()); // "13" - string 13 - heap 
+
+//if (userAge >= 13)
+//{
+//    Console.WriteLine("Welcome to Instagaram!");
+//}
+//else //(userAge < 13)
+//{
+//    Console.WriteLine("You are too young to use Instagaram!");
+//}
